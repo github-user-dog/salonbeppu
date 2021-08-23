7598664d467a92a37e60ec40d76ab7add4a762f3
Kohei <supposedly_pegasus@yahoo.co.jp>
2021-08-23 21:07:15 +0900

    second-commit

--- hooks-snake-game/src/utils/index.js
+++ hooks-snake-game/src/utils/index.js
@@ -1,4 +1,5 @@
 export const getFoodPosition = (fieldSize, excludes) => {
+
     while (true) {
         const x = Math.floor(Math.random() * (fieldSize - 1 - 1)) + 1;
         const y = Math.floor(Math.random() * (fieldSize - 1 - 1)) + 1;
@@ -8,11 +9,13 @@ export const getFoodPosition = (fieldSize, excludes) => {
             return { x, y };
         }
     }
+
+
 }
 
 export const initFields = (fieldSize, snake) => {
     const fields = []
-    for (let i = 0; i < fieldSize; i++) {
+    for (let i = 0; i < fieldSize; i) {
         const cols = new Array(fieldSize).fill('')
         fields.push(cols)
     }
@@ -21,4 +24,22 @@ export const initFields = (fieldSize, snake) => {
     const food = getFoodPosition(fieldSize, [snake])
     fields[food.y][food.x] = 'food'
     return fields
+}
+
+export const isCollision = (fieldSize, position) => {
+    if (position.y < 0 || position.x < 0) {
+        // x, y のどちらかの座標がマイナスの値 の時
+        return true;
+    }
+
+    if (position.y > fieldSize - 1 || position.x > fieldSize - 1) {
+        // x, y のどちらかの座標がフィールドサイズを超えてしまっている時
+        return true;
+    }
+
+    return false;
+};
+
+export const isEatingMyself = (fields, position) => {
+    return fields[position.y][position.x] === 'snake'
 }
